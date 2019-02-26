c5d9be39137207f7ae81c86ac07808564a61cb87
Attila Csoma <acsoma@leannet.eu>
2019-02-26 10:44:21 +0100

    Reformat with gofmt. No code change

--- response.go
+++ response.go
@@ -2,58 +2,57 @@ package main
 
 //This file contains the JSON response object (structs)
 
-//Written by Megyo @ LeanNet 
+//Written by Megyo @ LeanNet
 
 //import "time"
 
 type Flow struct {
-    Cookie    	string    	`json:"cookie"`
-    Duration  	string   	`json:"duration"`
-    Table     	string 		`json:"table"`
-    Packets   	string		`json:"packets"`
-    Bytes     	string		`json:"bytes"`
-    IdleTimeout string		`json:"idletimeout"` 
-    IdleAge		string		`json:"idleage"`
-    Priority	string		`json:"proirity"`
-    Match		string		`json:"match"`
-    Action		string		`json:"action"`
+	Cookie      string `json:"cookie"`
+	Duration    string `json:"duration"`
+	Table       string `json:"table"`
+	Packets     string `json:"packets"`
+	Bytes       string `json:"bytes"`
+	IdleTimeout string `json:"idletimeout"`
+	IdleAge     string `json:"idleage"`
+	Priority    string `json:"proirity"`
+	Match       string `json:"match"`
+	Action      string `json:"action"`
 }
 
 type Flows []Flow
 
 type Port struct {
-    PortNumber   string    	`json:"portnumber"`
-    RxPackets  	 string   	`json:"rxpackets"`
-    TxPackets    string 	`json:"txpackets"`
-    RxBytes   	 string		`json:"rxbytes"`
-    TxBytes      string		`json:"txbytes"`
-    RxDrops	 	 string		`json:"rxdrops"` 
-    TxDrops		 string		`json:"txdrops"`
-    RxErrors	 string		`json:"rxerrors"`
-    TxErrors	 string		`json:"txerrors"`
-    RxFrameErr	 string		`json:"rxframeerr"`
-    RxOverruns	 string		`json:"rxovverruns"`
-    RxCrcErrors	 string		`json:"rxcrcerrors"`
-    TxCollisions string		`json:"txcollisions"`
+	PortNumber   string `json:"portnumber"`
+	RxPackets    string `json:"rxpackets"`
+	TxPackets    string `json:"txpackets"`
+	RxBytes      string `json:"rxbytes"`
+	TxBytes      string `json:"txbytes"`
+	RxDrops      string `json:"rxdrops"`
+	TxDrops      string `json:"txdrops"`
+	RxErrors     string `json:"rxerrors"`
+	TxErrors     string `json:"txerrors"`
+	RxFrameErr   string `json:"rxframeerr"`
+	RxOverruns   string `json:"rxovverruns"`
+	RxCrcErrors  string `json:"rxcrcerrors"`
+	TxCollisions string `json:"txcollisions"`
 }
 
 type Ports []Port
 
 type Group struct {
-    GroupId   string    `json:"groupid"`
-    GroupType string   	`json:"grouptype"`
-    Buckets   []Bucket 	`json:"buckets"` 
-    Duration  string	`json:"duration"`
-    Bytes     string	`json:"bytes"`
-    Packets   string    `json:"packets"`
+	GroupId   string   `json:"groupid"`
+	GroupType string   `json:"grouptype"`
+	Buckets   []Bucket `json:"buckets"`
+	Duration  string   `json:"duration"`
+	Bytes     string   `json:"bytes"`
+	Packets   string   `json:"packets"`
 }
 
 type Bucket struct {
-//    BucketId  string    `json:"bucketid"` // for now I see no real usage of BucketID
-    Actions   string 	`json:"actions"` 
-    Bytes     string	`json:"bytes"`
-    Packets   string    `json:"packets"`
+	//    BucketId  string    `json:"bucketid"` // for now I see no real usage of BucketID
+	Actions string `json:"actions"`
+	Bytes   string `json:"bytes"`
+	Packets string `json:"packets"`
 }
 
 type Groups []Group
-
--- routes.go
+++ routes.go
@@ -3,19 +3,19 @@ package main
 import "net/http"
 
 type Route struct {
-    Name        string
-    Method      string
-    Pattern     string
-    HandlerFunc http.HandlerFunc
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
 }
 
 type Routes []Route
 
 var routes = Routes{
-    Route{
-        "Metrics",
-        "GET",
-        "/metrics",
-        GetMetrics,
-    },
+	Route{
+		"Metrics",
+		"GET",
+		"/metrics",
+		GetMetrics,
+	},
 }
