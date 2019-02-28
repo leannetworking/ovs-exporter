3acbeed3ab3d27cd771d3ff0a28ca1e6289ae3b9
Attila Csoma <acsoma@leannet.eu>
2019-02-28 16:49:41 +0100

    Use Prometheus lib to display measurements

--- main.go
+++ main.go
@@ -10,14 +10,28 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 //the TCP port that this scripts listens
 var listenPort string = ":8081"
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	target := r.URL.Query()["target"][0]
+	if target == "" {
+		http.Error(w, "Bad request!\nCorrect format is: http://<IP>:<Port>/flows?target=<targetIP>", 400)
+		return
+	}
+	c := collector{ip: target, port: ovsPort}
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(c)
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+}
+
 func main() {
-
-	router := NewRouter()
-
-	log.Fatal(http.ListenAndServe(listenPort, router))
+	http.HandleFunc("/test", handler)
+	log.Fatal(http.ListenAndServe(listenPort, nil))
 }
